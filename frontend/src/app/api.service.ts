f1f6f3f0fc0b934648b3ebb4f157c44941f9698e
Zibele <Zibele.nqunqa@gmail.com>
2021-02-16 23:46:55 +0200

    api

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,10 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Product }  from './product';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
   constructor(private httpClient:HttpClient) { }
+
+  PHP_API_SERVER = "http://127.0.0.1:8080";
 }
