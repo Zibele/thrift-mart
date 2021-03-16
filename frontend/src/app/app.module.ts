3b5a01638c353d63f5d4aabe55727499d743eb4e
Zibele <Zibele.nqunqa@gmail.com>
2021-03-16 21:53:54 +0200

    Dynamic header update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from '@angular/cdk/layout';
+
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { StoreHeaderComponent } from './store-header/store-header.component';
 
 @NgModule({
   declarations: [
@@ -17,13 +20,15 @@ import { HomeComponent } from './home/home.component';
     DashboardComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    StoreHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit{
 
   private _menuToggle : boolean;
+  private readonly _LARGE_SCREEN : number = 1024;
+  public mobile : boolean;
 
-  constructor() { this._menuToggle = false}
-
-  ngOnInit(): void {
+  constructor(public mediaMatcher : MediaMatcher) { 
+  
+    this._menuToggle = false
     
+    this.mobile = false;
+  }
+
+  
+  ngOnInit(): void {
+  
   }
 
   public get isToggled() {
@@ -22,5 +31,12 @@ export class HeaderComponent implements OnInit {
   public toggle(){
     this._menuToggle ? this._menuToggle = false : this._menuToggle = true;
   }
+
+  @HostListener("window:resize",[])
+  public onResize(){
+    this.mobile=window.screen.width < this._LARGE_SCREEN;
+  }
+
+  
 
 }
