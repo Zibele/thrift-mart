2843aba446e314b7496049d8f9f34649d048de2d
Zibele <Zibele.nqunqa@gmail.com>
2021-06-08 14:08:59 +0200

    Implemented ORM models

--- backend/ecommerceApis/catalog/models.py
+++ backend/ecommerceApis/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
+from django.contrib.auth.models import User
 
 class Role(models.Model):
     
@@ -20,24 +21,6 @@ class Role(models.Model):
     def __str__(self):
         return self.get_id_display()
 
-class User(AbstractUser):
-    roles = models.ManyToManyField(Role)
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    address = models.ForeignKey(Address, on_delete = models.SET_NULL)
-
-    def __str__(self):
-        return self.user.username
-   
-
-class Address(models.Model):
-    country = models.ForeignKey(Country, on_delete = models.SET_NULL)
-    address = models.TextField()
-    
-    def __str__(self):
-        return "%s, %s" % (address,country) 
-
 class Country(models.Model):
 
     SOUTH_AFRICA = 1
@@ -53,19 +36,24 @@ class Country(models.Model):
     def __str__(self):
         return self.get_id_display()
 
-class Product(models.Model):
-    profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
-    title = models.CharField(30)
-    description = models.TextField(80)
-    product_type = models.ForeighKey(ProductType, on_delete = models.SET_NULL)
-    gender_category = models.ForeignKey(ProductGenderCategory, on_delete = models.SET_NULL)
-    price = models.PositiveIntegerField(default = 50, validators = [MinValueValidator(50),MaxValueValidator(1000)])
-    quantity_in_stock = models.BooleanField(default = False)
-    date_posted = models.DateTimeField(auto_now_add=True)
+
+class Address(models.Model):
+    country = models.ForeignKey(Country, on_delete = models.SET_NULL, null = True)
+    address = models.TextField()
+    
+    def __str__(self):
+        return f"{address},{country}" 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    address = models.ForeignKey(Address, on_delete = models.SET_NULL , null=True)
+    roles = models.ManyToManyField(Role)
+    email = models.EmailField()
 
     def __str__(self):
-        return self.title
-
+        return self.user.username
+   
+   
 class ProductType(models.Model):
     TOP = 1
     BOTTOM = 2
@@ -77,11 +65,11 @@ class ProductType(models.Model):
         (TOP,"Tops"),
         (BOTTOM, "Bottoms"),
         (DRESS, "Dresses"),
-        (FOOT_WEAR, "Foot wear")
+        (FOOT_WEAR, "Foot wear"),
         (OUT_WEAR, "Out Wear")
         )
     
-    id = models.PrimitiveSmallInteger(choices = CHOICES,primary_key=True)
+    id = models.PositiveSmallIntegerField(choices = CHOICES,primary_key=True)
     
     def __str__(self):
         return self.get_id_display()
@@ -97,14 +85,30 @@ class ProductGenderCategory(models.Model):
         (UNISEX,"Unisex"),
         )
 
-    id = models.PositiveSmallInteger(choices = CHOICES, primary_key = True)
+    id = models.PositiveSmallIntegerField(choices = CHOICES, primary_key = True)
 
     def __str__(self):
         return self.get_id_display()
 
 
+class Product(models.Model):
+    profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField(80)
+    product_type = models.ForeignKey(ProductType, on_delete = models.SET_NULL, null = True)
+    gender_category = models.ForeignKey(ProductGenderCategory, on_delete = models.SET_NULL, null = True)
+    price = models.DecimalField(max_digits = 7, decimal_places = 2,validators = [MinValueValidator(0.01),MaxValueValidator(10000.00)])
+    quantity_in_stock = models.PositiveIntegerField(default = 0, validators = [MinValueValidator(0)])
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - R {self.price}"
+
+
+
+
 class Order(models.Model):
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    user = models.ForeignKey(Profile,on_delete = models.CASCADE)
     products = models.ManyToManyField(Product)
     quantity = models.PositiveIntegerField()
 
@@ -125,7 +129,7 @@ class OrderStatus(models.Model):
         (ORDER_CANCELLED,"Cancelled"),
     )
 
-    id = models.PositiveSmallInteger(choices = ORDER_STATUS, primary_key = True ) 
+    id = models.PositiveSmallIntegerField(choices = ORDER_STATUS, primary_key = True ) 
 
     def __str__(self):
         return self.get_id_display()
